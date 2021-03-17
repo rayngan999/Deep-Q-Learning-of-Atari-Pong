399420bc86a2fa53f3cf3505faabf7bbbf3a7629
Ray <rayngan999@gmail.com>
2021-03-17 00:16:13 +0000

    m

--- Starter Code/run_dqn_pong.py
+++ Starter Code/run_dqn_pong.py
@@ -20,7 +20,7 @@ env = wrap_pytorch(env)
 #Amount of episodes
 num_frames = 1000000
 batch_size = 32
-gamma = 0.99
+gamma = 0.9
 record_idx = 10000
 
 # Replay buffer
@@ -29,7 +29,7 @@ record_idx = 10000
 replay_initial = 10000
 replay_buffer = ReplayBuffer(100000)
 model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)
-model.load_state_dict(torch.load("model_trained.pth", map_location='cpu'))
+model.load_state_dict(torch.load("model_pretrained.pth", map_location='cpu'))
 
 
 target_model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)
@@ -41,7 +41,7 @@ if USE_CUDA:
     target_model = target_model.cuda()
     print("Using cuda")
 
-epsilon_start = 0.01
+epsilon_start = 1.0
 epsilon_final = 0.01
 epsilon_decay = 30000
 epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
@@ -69,9 +69,9 @@ for frame_idx in range(1, num_frames + 1):
         state = env.reset()
         all_rewards.append((frame_idx, episode_reward))
         episode_reward = 0
-        with open ("rewards_1m.txt", "a") as f:
-            line = str(all_rewards[-1][0]) + " : " + str(np.mean(all_rewards[-10:], 0)[1])
-            f.write('%s\n' %  line) 
+        #with open ("rewards_2m.txt", "a") as f:
+           # line = str(all_rewards[-1][0]) + " : " + str(np.mean(all_rewards[-10:], 0)[1])
+            #f.write('%s\n' %  line) 
     
     if len(replay_buffer) > replay_initial:
         loss = compute_td_loss(model, target_model, batch_size, gamma, replay_buffer)
@@ -79,8 +79,8 @@ for frame_idx in range(1, num_frames + 1):
         loss.backward()
         optimizer.step()
         losses.append((frame_idx, loss.data.cpu().numpy()))
-        with open ("losses_1m.txt", "a") as f:
-            line = str(frame_idx) + " : " + str(np.mean(losses, 0)[1])
+        with open ("losses_redo.txt", "a") as f:
+            line = str(frame_idx) + " : " + str(losses[-1][1])
             f.write('%s\n' % line)
     
     if frame_idx % 10000 == 0 and len(replay_buffer) <= replay_initial:
@@ -89,7 +89,7 @@ for frame_idx in range(1, num_frames + 1):
     if frame_idx % 10000 == 0 and len(replay_buffer) > replay_initial:
         print('#Frame: %d, Loss: %f' % (frame_idx, np.mean(losses, 0)[1]))
         print('Last-10 average reward: %f' % np.mean(all_rewards[-10:], 0)[1])
-        torch.save(model.state_dict(), "model_trained.pth")
+        #torch.save(model.state_dict(), "model_trained.pth")
         losses = []
         all_rewards = []
         
